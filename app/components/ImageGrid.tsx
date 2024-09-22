ebf80f6a257cf9c6f97d2f0de2feeaf968c670a1
hi-asamin <asamin.itconsultant@gmail.com>
2024-09-22 10:38:02 +0900

    イメージグリッドのレスポンシブ化

--- app/components/ImageGrid.tsx
+++ app/components/ImageGrid.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export const ImageGrid = ({ images }: Props): React.JSX.Element => {
   return (
-    <div className="grid grid-cols-2 gap-4 p-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
       {images.map((image) => (
         <div
           key={image.id}
